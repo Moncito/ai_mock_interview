35d1aa5c932bbdacb2e0687f74fa8f4f5704df8f
Moncito <moncitoglenn03@gmail.com>
2025-03-31 16:30:45 +0800

    implement interview session

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -131,33 +131,3 @@ export async function isAuthenticated() {
 
     return !!user; // Returns true if user exists, false otherwise
 }
-
-export async function getInterviewByUserId(userId: string): Promise<Interview [] | null>{
-    const interviews = await db
-    .collection('interview')
-    .where('userId', '==', userId)
-    .orderBy('createdAt', 'desc')
-    .get();
-
-    return interviews.docs.map((doc)  => ({
-        id: doc.id,
-        ...doc.data()
-    }))as Interview[]
-}
-
-export async function getLatestInterviews(params: GetLatestInterviewsParams): Promise<Interview [] | null>{
-    const {userId, limit = 20} = params;
-
-    const interviews = await db
-    .collection('interviews')
-    .orderBy('createdAt', 'desc')
-    .where('finalized', '==', true)
-    .where('userId', '!=', userId)
-    .limit(limit)
-    .get();
-
-    return interviews.docs.map((doc)  => ({
-        id: doc.id,
-        ...doc.data()
-    }))as Interview[]
-}
