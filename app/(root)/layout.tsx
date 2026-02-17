7564d00b80707066c174b6f61840ea228f1ccdfa
potchii <benitezernest33@gmail.com>
2026-02-17 23:50:19 +0800

    feature: add logout functionality

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
 import { isAuthenticated } from "@/lib/actions/auth.action";
+import { LogoutButton } from "@/components/LogoutButton";
 import Image from "next/image";
 import Link from "next/link";
 import { redirect } from "next/navigation";
@@ -11,11 +12,18 @@ const Rootlayout = async ({ children }: { children: ReactNode }) => {
 
     return (
         <div className="root-layout">
-            <nav>
+            <nav className="flex items-center justify-between">
                 <Link href="/" className="flex items-center gap-2">
                     <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
                     <h2 className="text-primary-100">PrepSaint</h2>
                 </Link>
+
+                <LogoutButton
+                    variant="outline"
+                    size="default"
+                    showIcon={true}
+                    showText={true}
+                />
             </nav>
             {children}
         </div>
